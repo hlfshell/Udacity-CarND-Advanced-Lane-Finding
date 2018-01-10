e8c14b7130ed454a49f0ba98fbb116fb22c0aedf
Keith Chester <kchester@thisisfusion.com>
2018-01-10 13:42:41 -0600

    init: Planning out how to approach pipeline.py

---
+++ pipeline.py
@@ -0,0 +1,26 @@
+
+
+# given an image, undistory per camera calibration
+def undistort(img):
+    pass
+ 
+# perspective transfomr
+def perspectiveTransform(img):
+    pass
+
+#pipeline - accepts an image, returns ???
+def pipeline(img):
+
+    # Undistort image
+
+    # Color / gradient threshold
+
+    # Perspective transform
+
+    #  Detect lane lines
+
+    # Calculate lane curvature 
+
+    # Calculate distance from center for car
+
+    pass
